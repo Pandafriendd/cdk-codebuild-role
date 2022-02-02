91c247f97d707584040232f9fc157792e4d5a49d
EC2 Default User <ec2-user@ip-172-31-20-19.us-east-2.compute.internal>
2022-02-02 21:23:38 +0000

    first commit

--- lib/cdk-codebuild-role-stack.ts
+++ lib/cdk-codebuild-role-stack.ts
@@ -1,16 +1,48 @@
 import { Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+
+import * as codebuild from 'aws-cdk-lib/aws-codebuild';
+import * as iam from 'aws-cdk-lib/aws-iam';
 
 export class CdkCodebuildRoleStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'CdkCodebuildRoleQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const invalidateBuildProject = new codebuild.PipelineProject(this, `InvalidateProject`, {
+      buildSpec: codebuild.BuildSpec.fromObject({
+        version: '0.2',
+        phases: {
+          build: {
+            commands:[
+              'aws cloudfront create-invalidation --distribution-id ${CLOUDFRONT_ID} --paths "/*"',
+            ],
+          },
+        },
+      }),
+      role: this.createBuildRole('666'),
+      grantReportGroupPermissions: false,
+    });
   }
+  
+  createBuildRole = (name: string): iam.IRole => {
+    const role = new iam.Role(this, 'testrole6666', {
+      assumedBy: new iam.CompositePrincipal(
+        new iam.ServicePrincipal('codebuild.amazonaws.com'),
+        new iam.ServicePrincipal('codepipeline.amazonaws.com'),
+        new iam.ServicePrincipal('cloudformation.amazonaws.com'),
+      ),
+    });
+    role.addToPolicy(
+      new iam.PolicyStatement({
+        effect: iam.Effect.ALLOW,
+        resources: ['*'],
+        actions: [
+          'eks:*'
+        ],
+      }),
+    );
+    return role;
+  };
+  
+  
 }
