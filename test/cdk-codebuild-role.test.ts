ac3bceaf880661dbb07a319d192e9edf2b5ee61a
EC2 Default User <ec2-user@ip-172-31-20-19.us-east-2.compute.internal>
2022-02-02 20:33:37 +0000

    Initial commit

---
+++ test/cdk-codebuild-role.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as CdkCodebuildRole from '../lib/cdk-codebuild-role-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/cdk-codebuild-role-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new CdkCodebuildRole.CdkCodebuildRoleStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
